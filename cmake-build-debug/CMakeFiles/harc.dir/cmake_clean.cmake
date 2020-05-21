4ccba29381ec1f57dd617fbc02e4c054b5e2addf
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2020-05-21 15:25:58 +0700

    initial commit

---
+++ cmake-build-debug/CMakeFiles/harc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/harc.dir/main.c.o"
+  "harc"
+  "harc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/harc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
